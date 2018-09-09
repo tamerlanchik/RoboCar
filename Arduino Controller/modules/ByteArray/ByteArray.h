f30adb936aa5e47acc889490f7ef9112ec1d5a41
Ian <andreykochnov@yandex.ru>
2018-09-09 21:30:00 +0300

    Add ByteArray class

---
+++ Arduino Controller/modules/ByteArray/ByteArray.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_BYTEARRAY
+#define HEADER_BYTEARRAY
+
+class ByteArray{
+  public:
+    static void reverseString(byte a[], int l);
+
+    static float getFloatFromByteArray(byte t[], unsigned int start);
+    static int getIntFromByteArray(byte t[], unsigned int start);
+};
+
+#endif
